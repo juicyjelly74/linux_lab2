0b3bfe62a7b3d2c042d3d5e29c37faf546b1a8ea
Julia <juicy.jelly74@gmail.com>
2016-03-05 19:28:15 +0300

    added comment about mktime function

--- task2.c
+++ task2.c
@@ -10,7 +10,7 @@ int main(){
 	scanf("%d/%d/%d %d:%d", &timeStrct.tm_mday, &month, &year, &timeStrct.tm_hour, &timeStrct.tm_min); 
 	timeStrct.tm_year = year - 1900; 
 	timeStrct.tm_mon = month - 1; 
-	time_t timeBirth = mktime(&timeStrct); 
+	time_t timeBirth = mktime(&timeStrct); /* рассчитывает UTC время в секундах, истекшее с 0 часов 1 января 1970 года */ 
 	if (timeBirth == -1){ 
 		printf("Error!"); 
 	return -1; 
